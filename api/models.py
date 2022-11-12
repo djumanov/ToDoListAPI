587fb0c13a930fce5a67db6c8ffb8d2f38dd9e68
djumanov <diyorbekjumanov1120@gmail.com>
2022-11-13 00:53:41 +0500

    models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Task(models.Model):
+    '''Task schema'''
+
+    title = models.CharField(max_length=63)
+    description = models.CharField(max_length=255, blank=True, default='')
+    is_completed = models.BooleanField(default=False, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    duedate = models.DateTimeField()
+    importance = models.SmallIntegerField(
+        default=0,
+        blank=True,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(0)
+        ]
+    )
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
